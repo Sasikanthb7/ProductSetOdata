41b62aefefcee8107a59aa5bbb20bb9299072414
sasi kanth <sasikanthb7@gmail.com>
2025-07-04 04:47:02 +0000

    changes for search help

--- webapp/controller/product.controller.js
+++ webapp/controller/product.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/Fragment"
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
 ], 
-(Controller,JSONModel,Fragment) => {
+(Controller,JSONModel,Fragment,Filter,FilterOperator) => {
     "use strict";
 
     return Controller.extend("com.training.project1.controller.product", {
@@ -66,10 +68,41 @@ sap.ui.define([
                 }
                 this._pValueHelpDialog.then(function(oDialog) {
                     // Create a filter for the binding
-                    oDialog.getBinding("items").filter([new Filter("Name", FilterOperator.Contains, sInputValue)]);
+                    oDialog.getBinding("items").filter([new Filter("Productid", FilterOperator.Contains, sInputValue)]);
                     // Open ValueHelpDialog filtered by the input's value
                     oDialog.open(sInputValue);
                 });
+        },
+        onValueHelpSearch: function (oEvent) {
+			var sValue = oEvent.getParameter("value");
+			var oFilter = new Filter("Productid", FilterOperator.Contains, sValue);
+
+			oEvent.getSource().getBinding("items").filter([oFilter]);
+		},
+
+		onValueHelpClose: function (oEvent) {
+			var oSelectedItem = oEvent.getParameter("selectedItem");
+			oEvent.getSource().getBinding("items").filter([]);
+
+			if (!oSelectedItem) {
+				return;
+			}
+
+			this.byId("productInput").setValue(oSelectedItem.getTitle());
+            let Selected = oSelectedItem.getTitle();
+            let oModel = this.getOwnerComponent().getModel();
+            let sPath = "/zuiproductSet('"+Selected+"')";
+            oModel.read(sPath,{
+                success:function(oData,Response){
+                    let Jmodel=new sap.ui.model.json.JSONModel(oData);
+                    this.getView().setModel(Jmodel,"readModel");
+                    }.bind(this),
+                error:function(){
+    
+                }
+    
+            })
         }
+
     });
 });
